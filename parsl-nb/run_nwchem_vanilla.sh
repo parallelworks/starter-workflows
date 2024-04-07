8251f3e96c1f155a7d4743f066fcd1f2c1ca1ed7
Michael McQuade <giraffesyo@gmail.com>
2024-04-08 01:41:53 +0800

    feat!: remove unused starters

--- parsl-nb/run_nwchem_vanilla.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#first serial, then mpi
-#if [ -z "$(echo \"$*\" |grep np)" ];then
-#    sudo docker run -u $(id -u):$(id -g) -v $PWD:/data nwchemorg/nwchem-dev $*
-#if [ -z "$1" ];then
-#echo "Example: $0 h2o.nw"
-#exit
-#fi
-docker run -u $(id -u):$(id -g) -v $PWD:/data nwchemorg/nwchem-dev $*
--- parsl-nb/utils/genInputs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for f in /pw/storage/gcs_store/gw100_structures/*;do
-     echo "<option value=\"$f\">$(basename $f .xyz)</option>"
-done
